97d7efafdcd42717201f5df15d51fda3497b0300
sonnd93 <sonjapan7@gmail.com>
2022-11-15 17:23:58 +0700

    update bai tap

---
+++ Tren-lop/a.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+// Vong lap For --------------------------------
+
+// for (Khai bien ; dieu kien; thuc thi toan tu) {code hien thi gia tri}
+// 1 var chi chay 1 lan => 2 kiem tra dieu kien la hanh dong thu thi sau moi lan lap => thuc thi doan ma code 4 sau ngoac {}
+// 3 thuc thi doan ma thu 3 => kiem tra dieu kien 2 => thuc thi doan ma code 4
+// 1 - 2 - 4 - 3 - 2 - 4 - 3 - 2 - 4 - 3 - 2
+
+// for (var i = 1; i < 1000; i++){
+//   //code
+//     console.log(i);
+// }
+
+
+
+// do/while loop ----------------------------------------------------------------
+
+// khai bao bien
+
+
+// var i = 0;
+
+// do {
+//     i++;
+//     console.log(i);
+    
+//     // code
+// } while (i < 10);
+
+// bang cuu chuong cua 5 ----------------------------------------------------
+// for (var i = 1; i <= 10; i++) {
+//     var product = 5 * i;
+//     document.write("5 x " + i + " = " + product);
+//     document.write("<br/>");
+// }
+
+// â
+
+// for (i = 1; i <= 10; i++) {
+//     for (j = 1; j <= 5; j++) {
+//         document.write("5 x " + i + " = " + 5 * i);
+//         document.write("<br/>");
+//         break;
+//         document.write("- Số " + j);
+//         document.write("<br/>");
+
+
+//     }
+//     if (i == 5); {
+//         continue;
+//     }
+// }
---
+++ bai_tap/bai-tap-3/3.2/script.js
@@ -0,0 +1,12 @@
+let c = prompt("Nhiệt độ hiện tại là:")
+
+if (c > 100) {
+    alert("Hãy giảm nhiệt độ")
+
+}
+else if (c < 20) {
+    alert("Hãy tăng nhiệt độ")
+}
+    else {
+    alert("Nhiệt độ bình thường")
+}
